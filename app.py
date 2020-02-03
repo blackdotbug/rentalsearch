889ae2706b7bc6c378f76d89c6a2b904cb3b8972
Heather Bree <me@justheatherbree.com>
2020-02-03 11:05:51 -0800

    compare page dialed in, mongodb sessions

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, redirect, request, jsonify
+from flask import Flask, render_template, redirect, request, jsonify, url_for, session 
+from flask_session import Session
 from flask_pymongo import PyMongo
 import datetime
 from splinter import Browser
@@ -8,13 +9,19 @@ import json
 import requests
 import re
 import scrape_properties
-from config import accessToken
+from config import accessToken, SECRETKEY, MONGOURI, SESSTYPE, MONGODB, ENV, CHROMEDRIVER
+import sys
 
+sys.setrecursionlimit(8000)
 app = Flask(__name__)
-
-# Use flask_pymongo to set up mongo connection
-app.config["MONGO_URI"] = "mongodb://localhost:27017/rental_search"
+app.config.from_object(__name__)
+app.config['SECRET_KEY'] = SECRETKEY
+app.config['MONGO_URI'] = MONGOURI
+app.config['SESSION_TYPE'] = SESSTYPE
+app.config['SESSION_MONGODB_DB'] = MONGODB
+app.config['FLASK_ENV'] = ENV
 mongo = PyMongo(app)
+Session(app)
 
 @app.route("/")
 def index():
@@ -25,8 +32,13 @@ def index():
     rentals = list(mongo.db.rentals.find())
     for rental in rentals:
         rental['value'] = -99
-        if rental['rent'] and rental['sqft']:
+        if rental['rent'] and rental['sqft'] and type(rental['sqft']) == int:
+            rental['value'] = round(int(''.join(i for i in rental['rent'] if i.isdigit()))/rental['sqft'],2)
+        elif rental['rent'] and rental['sqft']:
             rental['value'] = round(int(''.join(i for i in rental['rent'] if i.isdigit()))/int(''.join(i for i in rental['sqft'] if i.isdigit())),2)
+
+    if not session.get("compare") is None:
+        session.pop("compare", None)
     return render_template("index.html", rentals=rentals, scrapes=scrapes)
 
 @app.route("/scrape")
@@ -37,6 +49,9 @@ def scrape():
     rental_data,scrape_stats = scrape_properties.scrape_all()
     for address, rental in rental_data.items():
         if rental['address']:
+            current = rentals.find_one({"address":rental['address']})
+            if current['user_update']:
+                rental['user_update'] = current['user_update']
             rentals.replace_one({"address":rental['address']}, rental, upsert=True)
     for site, stat in scrape_stats.items():
         scrapes.replace_one({'site':site}, {'site': site, 'stat': stat, 'stamp': now}, upsert=True)
@@ -51,41 +66,61 @@ def scrape_one():
     rental_data, scrape_stats = scrape_properties.scrape_all(link)
     for address, rental in rental_data.items():
         if rental['address']:
-            rentals.replace_one({"address":rental['address']}, rental, upsert=True)
+            current = rentals.find_one({"address":address})
+            if current['user_update']:
+                rental['user_update'] = current['user_update']
+            rentals.replace_one({"address":address}, rental, upsert=True)
     for site, stat in scrape_stats.items():
         scrapes.replace_one({'site':site}, {'site': site, 'stat': stat, 'stamp': now}, upsert=True)
     return redirect("/", code=302)
 
-# @app.route("/map")
-# def map_all():
-#     data = mongo.db.rentals.find()
-#     rentals = []
-#     for row in data:
-#         if 'lat' in row.keys():
-#             rental = {'address': row['address'], 'lat':row['lat'], 'lng':row['lng']}
-#             rentals.append(rental)
-#     return render_template("map.html", rentals=rentals, accesstoken=accessToken)
-
 @app.route("/compare", methods=['GET', 'POST'])
 def compare():
-    links = request.form.getlist('compare[]')
-    details = scrape_details(links)
-    rentals = list(mongo.db.rentals.find({'link': {'$in': links}}, {'_id': False}))
-    for url,detail_list in details.items():
+    if not session.get("compare") is None:
+        rentals = session.get("compare")
+    else:
+        links = request.form.getlist('compare[]')
+        details = scrape_details(links)
+        rentals = list(mongo.db.rentals.find({'link': {'$in': links}}, {'_id': False}))
+        for url,detail_list in details.items():
+            for rental in rentals:
+                if rental['link'] == url:
+                    if rental['sqft'] and int(rental['sqft']) > 0:
+                        rental['details'] = detail_list
+                    else:
+                        sqfts = list(set(get_sqft(rental['link'])))
+                        rental['details'] = detail_list + sqfts
         for rental in rentals:
-            if rental['link'] == url:
-                rental['details'] = detail_list
-    for rental in rentals:
-        rental['value'] = -99
-        if rental['rent'] and rental['sqft']:
-            rental['value'] = round(int(''.join(i for i in rental['rent'] if i.isdigit()))/int(''.join(i for i in rental['sqft'] if i.isdigit())),2)
+            rental['value'] = -99
+            if rental['rent'] and rental['sqft'] and type(rental['sqft']) == int:
+                rental['value'] = round(int(''.join(i for i in rental['rent'] if i.isdigit()))/rental['sqft'],2)
+            elif rental['rent'] and rental['sqft']:
+                rental['value'] = round(int(''.join(i for i in rental['rent'] if i.isdigit()))/int(''.join(i for i in rental['sqft'] if i.isdigit())),2)
+        session['compare'] = rentals
+        session.modified = True
+    print(rentals)
     return render_template("compare.html", compare=rentals, accesstoken=accessToken)
 
 def delete_listing(url):
     mongo.db.rentals.delete_one({'link':url})
 
+def get_sqft(url):
+    executable_path = {"executable_path": CHROMEDRIVER}
+    browser = Browser("chrome", **executable_path, headless=True)
+    browser.visit(url)
+    text = ['sqft', 'sq. ft.', 'sq ft', 'square footage', 'sq.ft.']
+    html = browser.html
+    soup = BeautifulSoup(html, "html.parser")
+    sqfts = []
+    for tag in soup.descendants:
+        if tag.string and '/>' not in tag.string and 'rtCommonProps' not in tag.string:
+            for word in text:
+                if re.search(rf'\b{word}\b', tag.string, flags=re.IGNORECASE):
+                    sqfts.append(tag.string)
+    return sqfts
+
 def scrape_details(rental_urls):
-    executable_path = {"executable_path": "C:/Users/Heather Bree/chromedriver_win32/chromedriver"}
+    executable_path = {"executable_path": CHROMEDRIVER}
     browser = Browser("chrome", **executable_path, headless=True)
     listings_details = {}
     for url in rental_urls:
@@ -103,7 +138,6 @@ def scrape_details(rental_urls):
             else:
                 if tag.string and '/>' not in tag.string and 'rtCommonProps' not in tag.string:
                     for word in keywords:
-                        # if word in tag.string.lower():
                         if re.search(rf'\b{word}\b', tag.string, flags=re.IGNORECASE):
                             details.append(tag.string)
         deduped_details = list(set(details))
@@ -111,7 +145,6 @@ def scrape_details(rental_urls):
             styled_detail = detail
             for word in keywords:
                 if re.search(rf'\b{word}\b', styled_detail, flags=re.IGNORECASE):
-                    # styled_detail = styled_detail.replace(word, f"<b>{word}</b>")
                     styled_detail = re.sub(rf'\b{word}\b', f'<b>{word}</b>', styled_detail, flags=re.IGNORECASE)
             deduped_details[i] = styled_detail
         listings_details[url] = deduped_details
@@ -120,6 +153,55 @@ def scrape_details(rental_urls):
             listings_details.pop(listing)
     return listings_details
 
+@app.route("/update", methods=['GET','POST'])
+def update_properties():
+    updates = request.form
+    keys = ['parking', 'laundry', 'dishwasher', 'transit', 'gasoven', 'fireplace']
+    user_updates = {}
+    sqft = 0
+    for value in updates:
+        if value in keys:
+            user_updates[value] = updates[value]
+        elif value == 'sqft':
+            sqft = int(updates['sqft'])
+    if sqft > 0:    
+        mongo.db.rentals.update_one({'address':updates['address']}, {'$set' : {'user_update': user_updates, 'sqft': sqft}})
+    else:
+        mongo.db.rentals.update_one({'address':updates['address']}, {'$set' : {'user_update': user_updates}})
+    
+    if not session.get("compare") is None:
+        for listing in session['compare']:
+            if listing['address'] == updates['address']:
+                listing['user_update'] = user_updates
+                if sqft:
+                    listing['sqft'] = sqft
+                session.modified = True
+    return redirect(url_for("compare"), code=302)
+
+@app.route("/clean")
+def clean():
+    executable_path = {"executable_path": CHROMEDRIVER}
+    browser = Browser("chrome", **executable_path, headless=True)
+    rentals = list(mongo.db.rentals.find())
+    listings = {}
+    for rental in rentals:
+        browser.visit(rental['link'])
+        text = "Sorry, the listing you are trying to access is no longer available."
+        present = browser.is_text_present(text, wait_time=5)
+        if present == True:
+            delete_listing(rental['link'])
+        # else: 
+        #     INVISIBLE_ELEMS = ('style', 'script', 'head', 'title')
+        #     RE_SPACES = re.compile(r'\s{3,}')
+        #     page = browser.html
+        #     soup = BeautifulSoup(page, "html.parser")
+        #     text = ' '.join([
+        #         s for s in soup.strings
+        #         if s.parent.name not in INVISIBLE_ELEMS
+        #     ])           
+        #     listings[rental['link']] = RE_SPACES.sub('  ', text)
+    return redirect("/", code=302)
+    
 @app.route("/saved", methods=['GET', 'POST'])
 def saved_properties():
     return render_template('saved.html')
